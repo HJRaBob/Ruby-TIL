8713125e01dc8bc6695735c28c2e3e32deb153c2
HJRaBob <kidma222@naver.com>
2017-12-21 09:41:42 +0000

    Day3

---
+++ week3/Day3/app/controllers/home_controller.rb
@@ -0,0 +1,42 @@
+class HomeController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+  end
+
+  def complete
+    #db에 저장
+    Post.create(:username => params[:username], :title => params[:title], :content=> params[:content])
+    redirect_to '/'
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.all
+  end
+
+  def destroy
+    puts "done"
+    Post.find(params[:id]).destroy
+    puts "done"
+    redirect_to '/'
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(:username => params[:username], :title => params[:title], :content => params[:content])
+    redirect_to "/show/#{params[:id]}"
+  end
+
+  def add_comment
+    Comment.create(:content => params[:content], :post_id =>params[:id])
+    redirect_to :back
+  end
+
+end
