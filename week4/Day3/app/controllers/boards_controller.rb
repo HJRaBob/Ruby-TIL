438b04587426917e4bb6d8513b376c49ea3cab8e
HJRaBob <kidma222@naver.com>
2018-01-02 09:33:33 +0000

    finish

---
+++ week4/Day3/app/controllers/boards_controller.rb
@@ -0,0 +1,61 @@
+class BoardsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  def index
+    @boards = Board.all
+  end
+  
+  def show
+    @board = Board.find(params[:id])
+    @like = Like.where(user_id: current_user.id,board: @board.id)
+  end
+
+  def new
+
+  end
+
+  def create
+    board = Board.create(
+      title: params[:title],
+      contents: params[:contents],
+      user_id: current_user.id
+    )
+    redirect_to "/boards/#{board.id}"
+  end
+  def edit
+    @board = Board.find(params[:id])
+    #params[:id] => 숫자만 넘어옴
+  end
+  def update
+    @board = Board.find(params[:id])
+    @board.update(
+      title: params[:title],
+      contents: params[:contents]
+    )
+    redirect_to "/boards/#{@board.id}"
+    # locahost:3000/boards/1
+    # locahost:3000/boards/2
+    # locahost:3000/boards/3
+  end
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to "/"
+  end
+
+  def like_board
+    user_like = Like.where(user_id: current_user.id, board_id: params[:id])
+    if user_like.count>0
+      user_like.first.destroy
+    else
+      Like.create(
+        user_id: current_user.id,
+        board_id: params[:id]
+      )
+    end  
+    @like = Board.find(params[:id]).likes.count
+  end
+
+  def create_comment
+    
+  end
+end
---
+++ week4/Day3/config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+  # '/'
+  root 'boards#index'
+  # CRUD
+  # C - new, create
+  get '/boards/new' => 'boards#new' # new
+  post '/boards' => 'boards#create' # create
+  # R - index, show
+  get '/boards' => 'boards#index' # index
+  get '/boards/:id' => 'boards#show' # show
+  # U - edit, update
+  get '/boards/:id/edit' => 'boards#edit' # edit
+  put '/boards/:id' => 'boards#update' # update
+  # D - delete
+  delete '/boards/:id' => 'boards#destroy' # destroy
+
+  # SIGN IN / UP / OUT
+  # sign in
+  get '/signin' => 'sessions#signin'
+  post '/signin' => 'sessions#user_signin'
+
+  # sign up
+  get '/signup' => 'sessions#signup'
+  post '/signup' => 'sessions#user_signup'
+
+  # sign out
+  delete '/signout' => 'sessions#signout'
+
+  post '/boards/:id/like' =>'boards#like_board'
+end
